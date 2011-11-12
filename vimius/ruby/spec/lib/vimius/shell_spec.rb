b877f3e1c2898a3384f05cba092341fe4499d295
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-12 01:44:03 +0100

    Vimius: Move all ruby files into their own repository (Gem).

--- vimius/ruby/spec/lib/vimius/shell_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Shell do
-  it { should respond_to :shell }
-
-  it "should include the shell method into the Vimius module" do
-    Vimius.should respond_to :shell
-  end
-end
